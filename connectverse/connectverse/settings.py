236f7a4b325fc32a258ad99f5ee398076c92f4d3
Nifdi Guliyev <87927314+Nifdi01@users.noreply.github.com>
2023-08-21 21:14:56 +0400

    CVGC-10: Setup Google Authentication API

--- connectverse/connectverse/settings.py
+++ connectverse/connectverse/settings.py
@@ -137,4 +137,21 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
+]
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '383133926277-6bbaughikv24s4qlif4hdu1n11a630k1.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+SOCIAL_AUTH_PIPELINE = [
+ 'social_core.pipeline.social_auth.social_details',
+ 'social_core.pipeline.social_auth.social_uid',
+ 'social_core.pipeline.social_auth.auth_allowed',
+ 'social_core.pipeline.social_auth.social_user',
+ 'social_core.pipeline.user.get_username',
+ 'social_core.pipeline.user.create_user',
+ 'account.authentication.create_profile',
+ 'social_core.pipeline.social_auth.associate_user',
+ 'social_core.pipeline.social_auth.load_extra_data',
+ 'social_core.pipeline.user.user_details',
 ]
